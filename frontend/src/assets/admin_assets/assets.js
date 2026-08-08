88b97d79b6aa07fcb308f0b0f72eaa27fd99dec9
Abdullahel Safayet <ahsafayet100@gmail.com>
2026-08-09 04:53:31 +0600

    Update assets.js

--- frontend/src/assets/admin_assets/assets.js
+++ frontend/src/assets/admin_assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-system-backend-sui2.onrender.com/'
+export const url = 'https://last-change-backend.onrender.com'
