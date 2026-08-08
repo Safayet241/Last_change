e8a232656243cdc16ec68740098571824936f5aa
Abdullahel Safayet <ahsafayet100@gmail.com>
2026-08-09 04:52:45 +0600

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -11,7 +11,7 @@ const StoreContextProvider = (props) => {
     const fetchFoodList = async () => {
         try {
             const response = await fetch(
-                "https://food-delivery-system-backend-sui2.onrender.com/"
+                "https://last-change-backend.onrender.com"
             );
 
             const result = await response.json();
